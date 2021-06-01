598f126c361c2afb6eee2376acfec4087499590e
humaxoid <satman69@mail.ru>
2021-06-01 14:43:32 +0300

    1.06.2021
    Кнопки на вэбсокете, SPIFFS и с обновлением через WiFi.

--- data/script.js
+++ data/script.js
@@ -1,57 +1,49 @@
-var gateway = `ws://${window.location.hostname}/ws`;
-var websocket;
-window.addEventListener('load', onLoad);
-
-function onLoad(event) {
-    initWebSocket();
-}
-
-function initWebSocket() {
+  var gateway = `ws://${window.location.hostname}/ws`;
+  var websocket;
+  window.addEventListener('load', onLoad);
+  function initWebSocket() {
     console.log('Trying to open a WebSocket connection...');
     websocket = new WebSocket(gateway);
     websocket.onopen    = onOpen;
     websocket.onclose   = onClose;
     websocket.onmessage = onMessage;
-}
-
-function onOpen(event) {
-    console.log('Connection opened');
-    websocket.send("states");
-}
+  }
   
-function onClose(event) {
+  function onOpen(event) {
+    console.log('Connection opened');
+  }
+  function onClose(event) {
     console.log('Connection closed');
     setTimeout(initWebSocket, 2000);
-} 
-
-function onMessage(event) {
-    var myObj = JSON.parse(event.data);
-            console.log(myObj);
-            for (i in myObj.gpios){
-                var output = myObj.gpios[i].output;
-                var state = myObj.gpios[i].state;
-                console.log(output);
-                console.log(state);
-                if (state == "1"){
-                    document.getElementById(output).checked = true;
-                    document.getElementById(output+"s").innerHTML = "ON";
-                }
-                else{
-                    document.getElementById(output).checked = false;
-                    document.getElementById(output+"s").innerHTML = "OFF";
-                }
-            }
-    console.log(event.data);
-}
-
-// Send Requests to Control GPIOs
-function toggleCheckbox (element) {
-    console.log(element.id);
-    websocket.send(element.id);
-    if (element.checked){
-        document.getElementById(element.id+"s").innerHTML = "ON";
+  }
+ 
+  function onMessage(event) { 
+    switch(event.data) {
+      case '0': document.getElementById("state1").innerHTML = "OFF"; break
+      case '1': document.getElementById("state1").innerHTML = "ON"; break
+      case '2': document.getElementById("state2").innerHTML = "OFF"; break
+      case '3': document.getElementById("state2").innerHTML = "ON"; break
+      case '4': document.getElementById("state3").innerHTML = "OFF"; break
+      case '5': document.getElementById("state3").innerHTML = "ON"; break
+	  case '6': document.getElementById("state4").innerHTML = "OFF"; break
+      case '7': document.getElementById("state4").innerHTML = "ON"; break
+	  case '8': document.getElementById("state5").innerHTML = "OFF"; break
+      case '9': document.getElementById("state5").innerHTML = "ON"; break
     }
-    else {
-        document.getElementById(element.id+"s").innerHTML = "OFF"; 
     }
-}
+  
+  function onLoad(event) {initWebSocket(); initButton();}
+
+  function initButton() {
+    document.getElementById('button1').addEventListener('click', toggle1);
+	document.getElementById('button2').addEventListener('click', toggle2);
+	document.getElementById('button3').addEventListener('click', toggle3);
+	document.getElementById('button4').addEventListener('click', toggle4);
+	document.getElementById('button5').addEventListener('click', toggle5);
+  }
+  
+  function toggle1(){websocket.send('toggle1');}
+  function toggle2(){websocket.send('toggle2');}
+  function toggle3(){websocket.send('toggle3');}
+  function toggle4(){websocket.send('toggle4');}
+  function toggle5(){websocket.send('toggle5');}
